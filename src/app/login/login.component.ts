5367ea83219f3e34592b1ad961cf4402078934e6
99Kavyasangadi27 <kavyasangadi53@gmail.com>
2024-05-28 16:45:26 +0530

    api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +11,31 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private apiService: ApiService) {
     this.loginForm = this.fb.group({
       username: ['', [Validators.required, Validators.minLength(3)]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required,]]
     });
   }
 
   onSubmit() {
     if (this.loginForm.valid) {
-      console.log(this.loginForm.value);
-      alert('Login successful');
+      const loginData = this.loginForm.value;
+      this.router.navigate(['/home']);
+
+      // this.apiService.login(loginData).subscribe(   
+      //   Response => {
+      // // if ( this.loginForm.controls['username'].value == 'admin' && this.loginForm.controls['password'].value == 'admin') {
+      //   console.log(this.loginForm.value);
+      //   this.router.navigate(['/home']);
+      // alert('Login successful');
+  //     },
+  //   error => {
+  //     console.error('Login failed', error);
+  //     alert('invalid username or password');
+  //   }
+  // );
+      
     } else {
       console.log('Form is invalid');
     }
@@ -33,6 +48,8 @@ export class LoginComponent {
   onSignUp() {
     this.router.navigate(['/signup']);
   }
+  
+
 }
 
 
